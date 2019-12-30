3cacf630a5b9887365f7fb913e55488e373b4066
gbolla <gbolla@gbolla-mac.local>
2019-12-30 12:31:04 -0700

    config csvs

---
+++ configs/shared.py
@@ -0,0 +1,9 @@
+
+txn_date_col_name = "txn_date"
+txn_description_col_name = "txn_description"
+txn_amount_col_name = "txn_amount"
+
+data_root_dir = "data"
+data_raw_dir = "data/raw"
+data_interim_dir = "data/interim"
+data_processed_dir = "data/processed"
---
+++ configs/usaa_config.py
@@ -0,0 +1,14 @@
+from shared import txn_date_col_name, txn_description_col_name, txn_amount_col_name
+
+usaa_config = {
+    "columns" : {
+        0 : "status",
+        1 : None,
+        2 : txn_date_col_name,
+        3 : None,
+        4 : txn_description_col_name,
+        5 : "categorization",
+        6 : txn_amount_col_name
+    },
+    "txn_amount_positive_bool" : False
+}
